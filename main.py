0f4f80668ab045fb4b3c804d9df5aa7090153d50
Volkan776 <volkan.yilmaz1@std.yeditepe.edu.tr>
2024-12-25 16:46:07 +0300

    trinked pyhton aday işe alım

---
+++ main.py
@@ -0,0 +1,6 @@
+yas = int(input("Adayın yaşını girin: "))
+final_notu = int(input("Adayın final notunu girin: "))
+if 20 <= yas <= 50 or final_notu >= 80:
+  print("ise al")
+else:
+  print("reddet")
